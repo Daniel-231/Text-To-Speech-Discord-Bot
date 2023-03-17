4e5c7186cbcae4276d3a6c1d59814f70e940e29d
Daniel-231 <77553553+Daniel-231@users.noreply.github.com>
2023-03-17 20:44:38 +0200

    Delete writeToFile.js

--- writeToFile.js
+++
@@ -1,8 +0,0 @@
-function writeToFile(message) {
-    const timestamp = new Date().toISOString();
-    const content = `${timestamp} - ${message.author.username}: ${message.content}\n`;
-    fs.appendFile("log.txt", content, (err) => {
-      if (err) throw err;
-    });
-    console.log(content);
-  }
